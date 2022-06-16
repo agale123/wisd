3e39307bd9fb7b11ae48b770162dad7b569f148e
Alison Gale <agale753@gmail.com>
2022-06-16 08:53:36 -0700

    Add hover and click events to show details of the pass on click

--- passes/passes.js
+++ passes/passes.js
@@ -3,16 +3,18 @@ const WIDTH = 120;
 const HEIGHT = 80;
 
 /** Scale factor to convert to pixels. */
-const SCALE = 5;
+const SCALE = 8;
 
 /** Margin around the field. */
 const MARGIN = 20;
+
+const STROKE_WIDTH = 2;
 
 /** Scale a point to the pixel location. */
 const norm = (x) => x * SCALE + MARGIN;
 
 // Create the svg element
-let svg = d3.select("body").append("svg")
+let svg = d3.select("#field").append("svg")
     .attr("width", WIDTH * SCALE + MARGIN * 2)
     .attr("height", HEIGHT * SCALE + MARGIN * 2);
 
@@ -26,38 +28,80 @@ svg.append("rect")
 
 // Add center circle
 svg.append("circle")
-    .attr('cx', norm(60))
-    .attr('cy', norm(40))
-    .attr('r', 50)
-    .attr("stroke-width", 1)
+    .attr('cx', norm(WIDTH / 2))
+    .attr('cy', norm(HEIGHT / 2))
+    .attr('r', 10 * SCALE)
+    .attr("stroke-width", STROKE_WIDTH)
     .attr("stroke", "white")
     .attr("fill", "none");
 
 // Draw lines on field
 d3.csv("data/field.csv").then(lines => {
-    svg.selectAll("line")
-    .data(lines)
-    .enter()
-    .append("line")
-    .attr("x1", d => norm(d.x1))
-    .attr("y1", d => norm(d.y1))
-    .attr("x2", d => norm(d.x2))
-    .attr("y2", d => norm(d.y2))
-    .attr("stroke-width", 1)
-    .attr("stroke", "white");
-})
+    svg.selectAll("field")
+        .data(lines)
+        .enter()
+        .append("line")
+        .attr("x1", d => norm(d.x1))
+        .attr("y1", d => norm(d.y1))
+        .attr("x2", d => norm(d.x2))
+        .attr("y2", d => norm(d.y2))
+        .attr("stroke-width", STROKE_WIDTH)
+        .attr("stroke", "white");
+});
 
 // Draw the passes after reading data
 d3.csv("data/passes.csv").then(passes => {
-    svg.selectAll("line")
+    // Store a mapping from index to d3 element
+    const index = {};
+
+    // Store the index of the currently selected line
+    let selected = undefined;
+
+    const stroke = (d) => d.outcome === "Complete" ? "blue" : "red";
+
+    // Shared attributes between the lines
+    const attrs = {
+        "x1": d => norm(d.location_x),
+        "y1": d => norm(d.location_y) ,
+        "x2": d => norm(d.location_end_x),
+        "y2": d => norm(d.location_end_y)
+    };
+
+    // Visual representation of the line
+    svg.append("g")
+        .selectAll("passes")
         .data(passes)
         .enter()
         .append("line")
         .filter(d => d.possession_team === "Italy")
-        .attr("x1", d => norm(d.location_x))
-        .attr("y1", d => norm(d.location_y))
-        .attr("x2", d => norm(d.location_end_x))
-        .attr("y2", d => norm(d.location_end_y))
-        .attr("stroke-width", 1)
-        .attr("stroke", d => d.outcome === "Complete" ? "blue" : "red");
-})
+        .attrs(attrs)
+        .attr("stroke-width", STROKE_WIDTH)
+        .attr("stroke", d => stroke(d))
+        .each(function (_, i) {
+            index[i] = this;
+        });
+    // Larger target for mouseover and click events
+    svg.append("g")
+        .selectAll("passesTarget")
+        .data(passes)
+        .enter()
+        .append("line")
+        .filter(d => d.possession_team === "Italy")
+        .attrs(attrs)
+        .attr("stroke-width", STROKE_WIDTH * 10)
+        .attr("stroke", "transparent")
+        .on("mouseover", (_, i) => {
+            d3.select(index[i]).attr("stroke-width", STROKE_WIDTH * 2);
+        })
+        .on("mouseout", (_, i) => {
+            d3.select(index[i]).attr("stroke-width", STROKE_WIDTH);
+        })
+        .on("click", (d, i) => {
+            if (selected) {
+                d3.select(index[selected]).attr("stroke", stroke(d));
+            }
+            selected = i;
+            d3.select(index[i]).attr("stroke", "yellow");
+            document.getElementById("details").innerHTML = JSON.stringify(d);
+        });
+});
