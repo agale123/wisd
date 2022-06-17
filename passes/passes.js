349aae0e91d13006f58289f0e803368ff403b0b6
Alison Gale <agale753@gmail.com>
2022-06-16 18:09:47 -0700

    Add players from 360 frames

--- passes/passes.js
+++ passes/passes.js
@@ -13,6 +13,19 @@ const STROKE_WIDTH = 2;
 /** Scale a point to the pixel location. */
 const norm = (x) => x * SCALE + MARGIN;
 
+/** Color an actor in a frame. */
+function actor(frame) {
+    if (frame['actor']) {
+        return frame['keeper'] ? 'indigo' : 'darkblue';
+    } else if (frame['teammate']) {
+        return  frame['keeper'] ? 'purple' : 'blue';
+    } else if (frame['keeper']) {
+        return 'orange';
+    } else {
+        return 'yellow';
+    }
+}
+
 // Create the svg element
 let svg = d3.select("#field").append("svg")
     .attr("width", WIDTH * SCALE + MARGIN * 2)
@@ -50,7 +63,46 @@ d3.csv("data/field.csv").then(lines => {
 });
 
 // Draw the passes after reading data
-d3.csv("data/passes.csv").then(passes => {
+Promise.all([d3.csv("data/passes.csv"), d3.csv("data/frames.csv")]).then(([passes, frames]) => {
+    // Prosses passes
+    const filteredPasses = passes.filter(p => {
+        return p.team === "Italy" && p.minute < 5;
+    });
+    // Process frames
+    const processedFrames = frames.reduce((acc, cur) => {
+        // Use proper JSON quotes and booleans
+        const frameString = cur['freeze_frame']
+            .replaceAll("'", '"')
+            .replaceAll("True", "true")
+            .replaceAll("False", "false");
+        acc[cur['event_uuid']] = JSON.parse(frameString);
+        return acc;
+    }, {});
+    // TODO(agale): Update pass start locations with more accurate frame data
+    renderPasses(filteredPasses, processedFrames);
+});
+
+/** Render a 360 frame. */
+function renderFrame(frame) {
+    console.log(frame);
+    svg.append("g")
+        .attr("class", "players")
+        .selectAll("players")
+        .data(frame)
+        .enter()
+        .append("circle")
+        .attr('cx', d => norm(d['location'][0]))
+        .attr('cy', d => norm(d['location'][1]))
+        .attr('r', SCALE)
+        .attr("fill", d => actor(d));
+}
+
+/** Clear the 360 frame. */
+function clearFrame() {
+    d3.select(".players").remove();
+}
+
+function renderPasses(passes, frames) {
     // Store a mapping from index to d3 element
     const index = {};
 
@@ -62,7 +114,7 @@ d3.csv("data/passes.csv").then(passes => {
     // Shared attributes between the lines
     const attrs = {
         "x1": d => norm(d.location_x),
-        "y1": d => norm(d.location_y) ,
+        "y1": d => norm(d.location_y),
         "x2": d => norm(d.location_end_x),
         "y2": d => norm(d.location_end_y)
     };
@@ -73,9 +125,9 @@ d3.csv("data/passes.csv").then(passes => {
         .data(passes)
         .enter()
         .append("line")
-        .filter(d => d.possession_team === "Italy")
         .attrs(attrs)
         .attr("stroke-width", STROKE_WIDTH)
+        .attr("stroke-linecap", "round")
         .attr("stroke", d => stroke(d))
         .each(function (_, i) {
             index[i] = this;
@@ -86,7 +138,6 @@ d3.csv("data/passes.csv").then(passes => {
         .data(passes)
         .enter()
         .append("line")
-        .filter(d => d.possession_team === "Italy")
         .attrs(attrs)
         .attr("stroke-width", STROKE_WIDTH * 10)
         .attr("stroke", "transparent")
@@ -98,10 +149,17 @@ d3.csv("data/passes.csv").then(passes => {
         })
         .on("click", (d, i) => {
             if (selected) {
-                d3.select(index[selected]).attr("stroke", stroke(d));
+                d3.select(index[selected]).attr("stroke-dasharray", "");
+                clearFrame();
             }
             selected = i;
-            d3.select(index[i]).attr("stroke", "yellow");
+            d3.select(index[i]).attr("stroke-dasharray", "2,5");
+            const frame = frames[d['id']];
+            if (frame) {
+                renderFrame(frame);
+            }
+
+            // TODO(agale): Render prettier details.
             document.getElementById("details").innerHTML = JSON.stringify(d);
         });
-});
+}
